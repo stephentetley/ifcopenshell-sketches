15cb48d97afdb9f01f51502ad8b67fb6574577ef
Stephen Tetley <stephen.tetley@gmail.com>
2026-01-11 13:56:34 +0000

    Added examples using add_railing_representation

---
+++ src/simple_railing.py
@@ -0,0 +1,71 @@
+import ifcopenshell.api.context
+import ifcopenshell.api.geometry
+import ifcopenshell.api.project
+import ifcopenshell.api.root
+import ifcopenshell.api.unit
+from ifcopenshell.util.shape_builder import V
+import numpy
+
+
+def make_placement_matrix(x, y, z): 
+    matrix = numpy.eye(4)
+    matrix[:,3][0:3] = (x, y, z)
+    return matrix
+
+def make_placement_angle_matrix(deg, x, y, z): 
+    matrix = numpy.eye(4)
+    matrix = ifcopenshell.util.placement.rotation(deg, "Z") @ matrix
+    matrix[:,3][0:3] = (x, y, z)
+    return matrix
+
+
+# Create a blank model
+ifcfile = ifcopenshell.api.project.create_file(version="IFC4X3")
+project = ifcopenshell.api.root.create_entity(file=ifcfile, 
+                                              ifc_class="IfcProject", 
+                                              name="Simple Railing Project")
+
+model3d = ifcopenshell.api.context.add_context(file=ifcfile, context_type="Model")
+
+length = ifcopenshell.api.unit.add_si_unit(file=ifcfile, 
+                                           unit_type="LENGTHUNIT", 
+                                           prefix="MILLI")
+ifcopenshell.api.unit.assign_unit(file=ifcfile, units=[length])
+
+body = ifcopenshell.api.context.add_context(file=ifcfile,
+                                            context_type="Model", 
+                                            context_identifier="Body", 
+                                            target_view="MODEL_VIEW", 
+                                            parent=model3d)
+## railing_1
+
+railing1 = ifcopenshell.api.root.create_entity(file=ifcfile,
+                                               ifc_class='IfcRailing', 
+                                               name='Railing_1', 
+                                               predefined_type='NOTDEFINED')
+
+rpath = [V(0, 0, 1000.0), V(1000.0, 0, 1000.0), V(2000.0, 0, 1000.0)]
+rrep = ifcopenshell.api.geometry.add_railing_representation(file=ifcfile,
+                                                            context=body,
+                                                            railing_type='WALL_MOUNTED_HANDRAIL',
+                                                            railing_path=rpath,
+                                                            height=1000.0,                                                        
+                                                            railing_diameter=50.0,
+                                                            support_spacing=700.0,
+                                                            clear_width=40.0)
+
+
+ifcopenshell.api.geometry.assign_representation(file=ifcfile, 
+                                                product=railing1, 
+                                                representation=rrep)
+
+
+placement_matrix = make_placement_matrix(0, 0, 0)
+
+ifcopenshell.api.geometry.edit_object_placement(file=ifcfile, 
+                                                product=railing1,
+                                                matrix=placement_matrix)
+
+# Write out to a file
+ifcfile.write("./output/simple_railing1.ifc")
+
